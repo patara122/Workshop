c5e5d0eab5d05f758395f1593913d9a42af09899
patara122 <patara.a@gmail.com>
2025-10-31 21:24:39 +0700

    react leaflet

--- client/src/components/map/MapView.jsx
+++ client/src/components/map/MapView.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { useMap } from 'react-leaflet/hooks'
+import 'leaflet/dist/leaflet.css'
+import { LayersControl } from 'react-leaflet'
 
 const MapView = () => {
   return (
     <div className='flex-1'>
-      MapView 
+      <MapContainer
+        className='h-full'
+        center={[13, 100]}
+        zoom={7}
+        scrollWheelZoom={true}
+      >
+        <LayersControl>
+          {/* 1. Basemap */}
+          <LayersControl.BaseLayer name='OSM' checked={true}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name='WorldImagery'>
+            <TileLayer
+              attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
+
+      </MapContainer>
     </div>
   )
 }
